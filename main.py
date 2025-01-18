d92bb4ece6863d5df672e92fee59a155dffd7e13
PEKPELI kevin <pekpelignimdoukevin@gmail.com>
2025-01-18 21:02:00 +0000

    base model from pydantic

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class coord(BaseModel):
+    lat: float
+    long: float
+    zoom: Optional[int]
 
 #get
 @app.get('/')
 def helloword():
     return {'message': 'Hello, World!'}
+
+# post
+
+@app.post('/coordonee/{propo}')
+async def position(propo:int,coord : coord):
+    return {'propo':propo,'location': coord.dic()}
+
+# put
+
+# @app.get ('/components/')
+# async def read_components(number: int,text: Optional [str]):
+#     return {'number': number, 'text': text}
